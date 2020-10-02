c0e7277063678daade77a2178381aa76be9e3f53
enock24 <enookimuu@gmail.com>
2020-10-02 11:48:44 +0300

    add array to js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-var maleNames =("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
-var femaleNames =("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama");
+var maleNames =("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame")
+var femaleNames =("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama")
 
 
 var yy = (Document.parseInt(getElementbyId="year").value)
-var dd = parseInt(Document(id="yDay"));
-var mm = parseInt(Document(id="yDay"));
-var cc = parseInt(parseInt(id="yDay"))
-var dayoftheWeek (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) "mod 7.";
+var dd = (Document.parseInt(getElementbyId="date").value)
+var mm = (Document.parseInt(getElementbyId="month").value)
+var cc = (Document.parseInt(getElementbyId="centuary").value)
