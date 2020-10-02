dfa01b2fc0e7293a9bb0b5a7e70a1d5ce13a22b1
enock24 <enookimuu@gmail.com>
2020-10-02 11:28:01 +0300

    add css folder

---
+++ main.js
@@ -0,0 +1,9 @@
+var maleNames =("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
+var femaleNames =("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama");
+
+
+var yy = (Document.parseInt(getElementbyId="year").value)
+var dd = parseInt(Document(id="yDay"));
+var mm = parseInt(Document(id="yDay"));
+var cc = parseInt(parseInt(id="yDay"))
+var dayoftheWeek (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) "mod 7.";
