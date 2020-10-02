94dcd33e2cf863ef5ab451bab50263a7e1da09b7
enock24 <enookimuu@gmail.com>
2020-10-02 22:15:32 +0300

    add fuction to js

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ var maleNames =("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame")
 var femaleNames =("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama")
 
 
-var yy = (Document.parseInt(getElementbyId="year").value)
-var dd = (Document.parseInt(getElementbyId="date").value)
-var mm = (Document.parseInt(getElementbyId="month").value)
-var cc = (Document.parseInt(getElementbyId="centuary").value)
-var myDay=  ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD
+function getName(){
+    var yy=parseInt(document.getElementById("Year").value);
+    var mm =parseInt(document.getElementById("month").value);
+    var dd=parseInt(document.getElementById("day").value);
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+    alert(yy)
+    
+}
