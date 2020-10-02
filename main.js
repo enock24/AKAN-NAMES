098d5d4ee1cf9cc341b1df16df0029150fb774cc
enock24 <enookimuu@gmail.com>
2020-10-02 12:02:56 +0300

    add some var to js

--- main.js
+++ main.js
@@ -6,3 +6,4 @@ var yy = (Document.parseInt(getElementbyId="year").value)
 var dd = (Document.parseInt(getElementbyId="date").value)
 var mm = (Document.parseInt(getElementbyId="month").value)
 var cc = (Document.parseInt(getElementbyId="centuary").value)
+var myDay=  ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD mod 7;
